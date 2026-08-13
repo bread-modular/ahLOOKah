12a5f9955239dc1dfe34d41f4e48911882639d5c
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2026-08-13 13:52:27 +0000

    Add reusable button system.

--- main.js
+++ main.js
@@ -46,6 +46,7 @@ import {
   sampleNoiseFloorDb,
   NOISE_CAPTURE_DEFAULT_SECONDS,
 } from './noise-floor.js';
+import { button } from './ui.js';
 
 // ---------------------------------------------------------------------------
 // Window roles
@@ -3110,9 +3111,12 @@ function renderScreenToolbar() {
 
   const toolbar = document.createElement('div');
   toolbar.id = 'screen-toolbar';
-  toolbar.innerHTML = `
-    <button id="open-control-btn" title="Open a control panel window"><svg class="btn-icon" viewBox="0 0 24 24" aria-hidden="true"><path d="M8 3H5a2 2 0 0 0-2 2v3"></path><path d="M21 8V5a2 2 0 0 0-2-2h-3"></path><path d="M16 21h3a2 2 0 0 0 2-2v-3"></path><path d="M3 16v3a2 2 0 0 0 2 2h3"></path></svg>Control Panel</button>
-  `;
+  toolbar.innerHTML = button({
+    id: 'open-control-btn',
+    icon: '<svg class="btn-icon" viewBox="0 0 24 24" aria-hidden="true"><path d="M8 3H5a2 2 0 0 0-2 2v3"></path><path d="M21 8V5a2 2 0 0 0-2-2h-3"></path><path d="M16 21h3a2 2 0 0 0 2-2v-3"></path><path d="M3 16v3a2 2 0 0 0 2 2h3"></path></svg>',
+    label: 'Control Panel',
+    title: 'Open a control panel window',
+  });
   document.body.appendChild(toolbar);
 
   toolbar.querySelector('#open-control-btn').onclick = () => openControlWindow();
